19150459fa5fbf0d2f3ac2406e8219d7bb87441a
ArsenSidorov <81903624+ArsenSidorov@users.noreply.github.com>
2021-04-09 13:24:40 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
 const swiper = new Swiper('.channel-slider', {
   // Optional parameters
   loop: true,
-slidesPerView: 6,
+slidesPerView: 1,
+spaceBetween: 20,
+breakpoints: {
+  1900: {
+    slidesPerView: 6
+  },
+  1500: {
+    slidesPerView: 5
+  },
+  1300: {
+    slidesPerView: 4
+  },
+  1100: {
+    slidesPerView: 3
+  },
+  760: {
+    slidesPerView: 2
+  },
+  480: {
+    slidesPerView: 1
+  }
+},
   navigation: {
     nextEl: '.channel-button-next',
     prevEl: '.channel-button-prev',
@@ -11,7 +32,19 @@ slidesPerView: 6,
 const swiper1 = new Swiper('.channel-slider-recommended', {
   // Optional parameters
   loop: true,
-slidesPerView: 3,
+slidesPerView: 1,
+spaceBetween: 20,
+breakpoints: {
+  1900: {
+    slidesPerView: 3
+  },
+  1200: {
+    slidesPerView: 2
+  },
+  760: {
+    slidesPerView: 1
+  },
+},
   navigation: {
     nextEl: '.channel-button-rec-next',
     prevEl: '.channel-button-rec-prev',
@@ -22,10 +55,42 @@ slidesPerView: 3,
 const swiper2 = new Swiper('.channel-fd-slider', {
   // Optional parameters
   loop: true,
-slidesPerView: 6,
+slidesPerView: 1,
+spaceBetween: 20,
+breakpoints: {
+  1900: {
+    slidesPerView: 6
+  },
+  1500: {
+    slidesPerView: 5
+  },
+  1300: {
+    slidesPerView: 4
+  },
+  1100: {
+    slidesPerView: 3
+  },
+  760: {
+    slidesPerView: 2
+  },
+  480: {
+    slidesPerView: 1
+  }
+},
   navigation: {
     nextEl: '.channel-button-fd-next',
     prevEl: '.channel-button-fd-prev',
   },
-
 });
+
+const searchBtn = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('input-group');
+searchBtn.addEventListener('click', () => {
+  mobileSearch.classList.toggle('is-open');
+});
+
+if (document.documentElement.scrollWidth <=640) {
+  swiper.destroy();
+  swiper1.destroy();
+  swiper2.destroy();
+};
